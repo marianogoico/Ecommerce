c6d3f5fd8241886e230f20da54c1d15fd245d01a
MarianoGoico <marianogoico@gmail.com>
2023-09-07 15:48:38 -0300

    3ra pre entrega

--- js/main.js
+++ js/main.js
@@ -1,65 +1,292 @@
-alert("Ingrese la opcion del producto que desea llevar, para salir ingrese 0")
-let seleccionarProductos = Number(prompt("1-buzo $3000 2-remera $1500 3-jean $5000 4-Zapatillas $6000 "))
-let seleccionarCantidad;
-let total = 0;
+// Productos
+const productos = [
+    // Abrigos
+    {
+        id: "abrigo-01",
+        titulo: "Abrigo 01",
+        imagen: "./img/abrigos/01.jpg",
+        categoria: {
+            nombre: "Abrigos",
+            id: "abrigos"
+        },
+        precio: 1000
+    },
+    {
+        id: "abrigo-02",
+        titulo: "Abrigo 02",
+        imagen: "./img/abrigos/02.jpg",
+        categoria: {
+            nombre: "Abrigos",
+            id: "abrigos"
+        },
+        precio: 1000
+    },
+    {
+        id: "abrigo-03",
+        titulo: "Abrigo 03",
+        imagen: "./img/abrigos/03.jpg",
+        categoria: {
+            nombre: "Abrigos",
+            id: "abrigos"
+        },
+        precio: 1000
+    },
+    {
+        id: "abrigo-04",
+        titulo: "Abrigo 04",
+        imagen: "./img/abrigos/04.jpg",
+        categoria: {
+            nombre: "Abrigos",
+            id: "abrigos"
+        },
+        precio: 1000
+    },
+    {
+        id: "abrigo-05",
+        titulo: "Abrigo 05",
+        imagen: "./img/abrigos/05.jpg",
+        categoria: {
+            nombre: "Abrigos",
+            id: "abrigos"
+        },
+        precio: 1000
+    },
+    // Camisetas
+    {
+        id: "camiseta-01",
+        titulo: "Camiseta 01",
+        imagen: "./img/camisetas/01.jpg",
+        categoria: {
+            nombre: "Camisetas",
+            id: "camisetas"
+        },
+        precio: 1000
+    },
+    {
+        id: "camiseta-02",
+        titulo: "Camiseta 02",
+        imagen: "./img/camisetas/02.jpg",
+        categoria: {
+            nombre: "Camisetas",
+            id: "camisetas"
+        },
+        precio: 1000
+    },
+    {
+        id: "camiseta-03",
+        titulo: "Camiseta 03",
+        imagen: "./img/camisetas/03.jpg",
+        categoria: {
+            nombre: "Camisetas",
+            id: "camisetas"
+        },
+        precio: 1000
+    },
+    {
+        id: "camiseta-04",
+        titulo: "Camiseta 04",
+        imagen: "./img/camisetas/04.jpg",
+        categoria: {
+            nombre: "Camisetas",
+            id: "camisetas"
+        },
+        precio: 1000
+    },
+    {
+        id: "camiseta-05",
+        titulo: "Camiseta 05",
+        imagen: "./img/camisetas/05.jpg",
+        categoria: {
+            nombre: "Camisetas",
+            id: "camisetas"
+        },
+        precio: 1000
+    },
+    {
+        id: "camiseta-06",
+        titulo: "Camiseta 06",
+        imagen: "./img/camisetas/06.jpg",
+        categoria: {
+            nombre: "Camisetas",
+            id: "camisetas"
+        },
+        precio: 1000
+    },
+    {
+        id: "camiseta-07",
+        titulo: "Camiseta 07",
+        imagen: "./img/camisetas/07.jpg",
+        categoria: {
+            nombre: "Camisetas",
+            id: "camisetas"
+        },
+        precio: 1000
+    },
+    {
+        id: "camiseta-08",
+        titulo: "Camiseta 08",
+        imagen: "./img/camisetas/08.jpg",
+        categoria: {
+            nombre: "Camisetas",
+            id: "camisetas"
+        },
+        precio: 1000
+    },
+    // Pantalones
+    {
+        id: "pantalon-01",
+        titulo: "Pantalón 01",
+        imagen: "./img/pantalones/01.jpg",
+        categoria: {
+            nombre: "Pantalones",
+            id: "pantalones"
+        },
+        precio: 1000
+    },
+    {
+        id: "pantalon-02",
+        titulo: "Pantalón 02",
+        imagen: "./img/pantalones/02.jpg",
+        categoria: {
+            nombre: "Pantalones",
+            id: "pantalones"
+        },
+        precio: 1000
+    },
+    {
+        id: "pantalon-03",
+        titulo: "Pantalón 03",
+        imagen: "./img/pantalones/03.jpg",
+        categoria: {
+            nombre: "Pantalones",
+            id: "pantalones"
+        },
+        precio: 1000
+    },
+    {
+        id: "pantalon-04",
+        titulo: "Pantalón 04",
+        imagen: "./img/pantalones/04.jpg",
+        categoria: {
+            nombre: "Pantalones",
+            id: "pantalones"
+        },
+        precio: 1000
+    },
+    {
+        id: "pantalon-05",
+        titulo: "Pantalón 05",
+        imagen: "./img/pantalones/05.jpg",
+        categoria: {
+            nombre: "Pantalones",
+            id: "pantalones"
+        },
+        precio: 1000
+    }
+];
 
+//Cosas que llamo del DOM
+const contenedorProductos = document.querySelector("#contenedor-productos");
+const botonesCategorias = document.querySelectorAll(".boton-categoria");
+const tituloPrincipal = document.querySelector("#titulo-principal")
+let botonesAgregar = document.querySelectorAll(".producto-agregar")
+const numerito = document.querySelector("#numerito")
 
-const cantidad = (cant, precio) => {
-    return cant * precio
+//Funcion para cargar todos los productos de mi array en la pagina
+function cargarProductos(productosElegidos) {
+
+    //vacio el contenedor de productos para evitar que se dupliquen
+    contenedorProductos.innerHTML = "";
+
+    productosElegidos.forEach(productos => {
+
+        //creo el html para los productos
+        const div = document.createElement("div");
+        div.classList.add("producto");
+        div.innerHTML = `
+            <img class="producto-imagen" src="${productos.imagen}" alt="${productos.titulo}">
+            <div class="producto-detalles">
+                <h3 class="producto-titulo">${productos.titulo}</h3>
+                <p class="producto-precio">$${productos.precio}</p>
+                <button class="producto-agregar" id="${productos.id}">Agregar</button>
+            </div>
+        `;
+
+        contenedorProductos.append(div);
+    })
+
+    actualizarBotonesAgregar();
 }
 
+cargarProductos(productos);
 
-while (seleccionarProductos != 0) {
-    switch (seleccionarProductos) {
-        case 1:
-            seleccionarCantidad = Number(prompt("el producto seleccionado es buzo, indique la cantidad"))
-            total += cantidad(seleccionarCantidad, 3000)
-            break;
-        case 2:
-            seleccionarCantidad = Number(prompt("el producto seleccionado es remera, indique la cantidad"))
-            total += cantidad(seleccionarCantidad, 1500)
-            break;
-        case 3:
-            seleccionarCantidad = Number(prompt("el producto seleccionado es jean, indique la cantidad"))
-            total += cantidad(seleccionarCantidad, 5000)
-            break;
-        case 4:
-            seleccionarCantidad = Number(prompt("el producto seleccionado es Zapatillas, indique la cantidad"))
-            total += cantidad(seleccionarCantidad, 6000)
-            break;
 
-        default:
-            break;
-    }
-    seleccionarProductos = Number(prompt("1-buzo $3000 2-remera $1500 3-jean $5000 4-Zapatillas $6000 "))
+//Mostrar solo la categoria de productos elegida
+botonesCategorias.forEach(boton => {
+    boton.addEventListener("click", (e) => {
+
+        //quito y agrego la clase active para que se vea bien el css
+        botonesCategorias.forEach(boton => boton.classList.remove("active"));
+        e.currentTarget.classList.add("active");
+
+
+        if (e.currentTarget.id != "todos") {
+
+            //cambio el titulo principal y filtro los productos segun la categoria elegida
+            const productoCategoria = productos.find(producto => producto.categoria.id === e.currentTarget.id);
+            tituloPrincipal.innerText = productoCategoria.categoria.nombre;
+            const productosBoton = productos.filter(producto => producto.categoria.id === e.currentTarget.id);
+            cargarProductos(productosBoton);
+
+        }
+        else {
+
+            tituloPrincipal.innerText = "Todos los Productos";
+            cargarProductos(productos);
+
+        }
+    })
+});
+
+
+//Actualizamos los botones de "agregar" y que estos ejecuten la funcion de agregar al carrito
+function actualizarBotonesAgregar() {
+    botonesAgregar = document.querySelectorAll(".producto-agregar");
+
+    botonesAgregar.forEach(boton => {
+        boton.addEventListener("click", agregarAlCarrito);
+    })
 }
 
-alert("el total de la compra es de: " + total)
+//Creo el array donde estaran los productos del carrito
+const productosEnCarrito = [];
 
 
-const envio = () => {
-    if (total >= 10000) {
-        alert("El envio es gratuito")
-    } else {
-        total += 1000
-        alert("el costo de envio es de 1000, el total es: " + total)
+//Creo la funcion que agrega los productos al carrito
+function agregarAlCarrito(e) {
+
+    const idBoton = e.currentTarget.id;
+    const productoAgregado = productos.find(producto => producto.id === idBoton);
+
+    //si el producto ya esta en el carrito, sumamos la cantidad
+    if (productosEnCarrito.some(producto => producto.id === idBoton)) {
+        const index = productosEnCarrito.findIndex(producto => producto.id === idBoton);
+        productosEnCarrito[index].cantidad++;
     }
+    //si no esta lo agregamos
+    else {
+        productoAgregado.cantidad = 1;
+        productosEnCarrito.push(productoAgregado);
+    }
+
+    actualizarNumerito();
+
+    localStorage.setItem("productos-en-carrito", JSON.stringify(productosEnCarrito));
+
+};
+
+//Funcion para actualizar el numero de productos en el carrito
+function actualizarNumerito() {
+    let nuevoNumerito = productosEnCarrito.reduce((acc, producto) => acc + producto.cantidad, 0);
+    numerito.innerText = nuevoNumerito;
 }
-
-const metodoDePago = () => {
-    let metodo = prompt("ingrese el metodo de pago, tarjeta o efectivo")
-    if (metodo == "tarjeta") {
-        total *= 1.1
-        alert("Se aplica un regarco por tarjeta, el total es: " + total);
-    }
-    else if (metodo == "efectivo") {
-        total -= 1000
-        alert("tenes un descuento de 1000, el total es:" + total)
-    }
-
-
-}
-
-envio()
-
-metodoDePago()
